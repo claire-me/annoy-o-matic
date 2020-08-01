63d872c3939d55ba496a993b6411f29970c9119f
anita-ihuman <charlesanita403@gmail.com>
2020-08-01 13:12:43 +0100

    My annoying game

---
+++ Annoy-O-Matic.js
@@ -0,0 +1,19 @@
+var secretNumber = 4;
+
+var answer = prompt("Guess a number")
+
+while (answer.indexOf("4") === -1) {
+    var answer =prompt("Try again")
+}
+var secretName = Love;
+var answer = prompt("Guess another  name")
+alert("YAY WE MADE IT")
+
+
+
+
+
+
+
+
+
